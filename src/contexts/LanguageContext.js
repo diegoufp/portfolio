ac5c559ba0aab56cd7b02546cd4da097df71b074
sann <diegofernandezp001@gmail.com>
2021-12-15 21:00:41 -0600

    Se ajusta la page de Contact

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -125,24 +125,101 @@ const icons = {
     }
 }
 
+const initialForm = {
+    name: "",
+    email: "",
+    subject:"",
+    comments:"" 
+};
+
+const translateForm={
+    es:{required:{
+        name:"El campo 'Nombre' es requerido",
+        email: "El campo 'Email' es requerido",
+        subject:"El campo 'Asunto a tratar' es requerido",
+        comments:"El campo 'Comentarios' es requerido"
+    },regex:{
+        name: "El campo 'Nombre' solo acepta letras y espacios en blanco",
+        email:"El campo 'Email' es incorrecto",
+        comments:"El campo 'Comentarios' no debe exceder los 255 caracteres"
+        
+    },placeholder:{
+        name:"Escribe tu nombre",
+        email:"Escribe tu email",
+        subject:"Asunto a tratar",
+        comments:"Escribe tus comentarios",
+        submit:"Enviar"
+    }},
+    en:{required:{
+        name:"The 'Name' field is required",
+        email:"The 'Email' field is required",
+        subject:"The field 'Subject to be addressed' is required",
+        comments:"The 'Comments' field is required"
+    }, regex:{
+        name:"The 'Name' field only accepts letters and blanks",
+        email:"The 'Email' field is incorrect",
+        comments:"The 'Comments' field must not exceed 255 characters"
+    },placeholder:{
+        name:"Enter your name",
+        email:"Write your email",
+        subject:"Issue at hand",
+        comments:"Write your comments",
+        submit:"Submit"
+    }}
+}
+
 const LanguageProvider = ({children})=>{
     const [language, setLanguage] = useState(initialLanguage);
     const [texts,setTexts] = useState(translations[language]);
-    const [sliderTexts, setSliderText] = useState(slider[language])
+    const [sliderTexts, setSliderText] = useState(slider[language]);
+    const [formText, setFormText] = useState(translateForm[language]);
 
     const handleLanguage = (e) =>{
         if(e.target.checked){
             setLanguage("es");
             setTexts(translations.es);
             setSliderText(slider.es);
+            setFormText(translateForm.es);
 
         }else{
             setLanguage("en");
             setTexts(translations.en);
             setSliderText(slider.en);
+            setFormText(translateForm.en);
         }
     };
-    const data={texts,handleLanguage, icons, sliderTexts};
+
+    const validationsForm = (form) =>{
+        let errors = {};
+        let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+        let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+        let regexComments = /^.{1,255}$/;
+    
+    
+        if(!form.name.trim()){
+            errors.name = true;     
+        }else if(!regexName.test(form.name.trim())){
+            errors.name = "regex";
+        };
+    
+        if(!form.email.trim()){
+            errors.email = true;
+        }else if(!regexEmail.test(form.email.trim())){
+            errors.email = "regex";
+        };
+    
+        if(!form.subject.trim()){
+            errors.subject = true;
+        };
+        if(!form.comments.trim()){
+            errors.comments = true;
+        }else if(!regexComments.test(form.comments.trim())){
+            errors.comments = "regex";
+        }
+    
+        return errors;
+    };
+    const data={texts,handleLanguage, icons, sliderTexts,initialForm,validationsForm,formText};
     return (
         <LanguageContext.Provider value={data}>
         {children}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import "../css/Contact.css"
+import React, { useContext } from 'react';
+import "../css/Contact.css";
+import ThemeContext from '../contexts/ThemeContext';
+import LanguageContext from '../contexts/LanguageContext';
+import { useForm } from '../hooks/useForm';
+
 
 const Contact = () => {
+    const {theme} = useContext(ThemeContext);
+    const {texts,initialForm,validationsForm,formText} = useContext(LanguageContext);
+
+    const {form,errors,loading,response,handleChange,handleBlur,handleSubmit} = useForm(initialForm,validationsForm);
     return (
-        <div>
-            <h3>Contact</h3>
+        <div className="main__contact">
+            <div className={`main__contact-content ${theme}`}>
+                <h2>{texts.contact}</h2>
+                <form onSubmit={handleSubmit}>
+                <input className={theme} type="text" name="name" placeholder={formText.placeholder.name} onBlur={handleBlur} onChange={handleChange} value={form.name} required/>
+                {(errors.name === "regex" && <p>{formText.regex.name}</p>)||(errors.name && <p>{formText.required.name}</p>)}
+
+                <input className={theme} type="email" name="email" placeholder={formText.placeholder.email} onBlur={handleBlur} onChange={handleChange} value={form.email} required/>
+                {(errors.email === "regex" && <p>{formText.regex.email}</p>)||(errors.email && <p>{formText.required.email}</p>)}
+
+                <input className={theme} type="text" name="subject" placeholder={formText.placeholder.subject} onBlur={handleBlur} onChange={handleChange} value={form.subject} required/>
+                {errors.subject && <p>{formText.required.subject}</p>}
+
+                <textarea className={theme} name="comments" cols="50" rows="5" placeholder={formText.placeholder.comments} onBlur={handleBlur} onChange={handleChange} value={form.comments} required></textarea>
+                {(errors.comments === "regex" && <p>{formText.regex.comments}</p>)||(errors.comments && <p>{formText.required.comments}</p>)}
+
+                <input className={theme} type="submit" value={formText.placeholder.submit}/>
+                </form>
+            </div>
         </div>
     )
 }
